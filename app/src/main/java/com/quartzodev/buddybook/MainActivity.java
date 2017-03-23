821d7162680b916fb981719fedcb4837c1c8002c
victoraldir <vhaldir@gmail.com>
2017-03-23 00:16:25 +0000

    Finishing authentication workflow. Using google and facebook as social authenticators

--- app/src/main/java/com/quartzodev/buddybook/MainActivity.java
+++ app/src/main/java/com/quartzodev/buddybook/MainActivity.java
@@ -1,25 +1,85 @@
 package com.quartzodev.buddybook;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.quartzo.transform.CircleTransform;
+
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 1;
+
+    @BindView(R.id.main_layout)
+    ConstraintLayout mConstraintLayout;
+    private ImageView mImageViewProfile;
+    private TextView mTextViewUsername;
+    private TextView mTextViewTextEmail;
+
+    @BindView(R.id.nav_view)
+    NavigationView mNavigationView;
+
+    private String mUserEmail;
+    private String mUsername;
+
+    private Context mContext;
+
+    //Authentication entities
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mContext = this;
+
+        LinearLayout linearLayout = (LinearLayout) mNavigationView.getHeaderView(0); //LinearLayout Index
+        mImageViewProfile = (ImageView) linearLayout.findViewById(R.id.main_imageview_user_photo);
+        mTextViewUsername = (TextView) linearLayout.findViewById(R.id.main_textview_username);
+        mTextViewTextEmail = (TextView) linearLayout.findViewById(R.id.main_textview_user_email);
+
+        //Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,6 +100,87 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user == null){
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }else{
+
+                    onSignedIn(user);
+
+                }
+            }
+        };
+    }
+
+
+    public void onSignedIn(FirebaseUser user){
+
+        mUserEmail = user.getEmail();
+        mUsername = user.getDisplayName();
+        Uri profileUri = user.getPhotoUrl();
+
+        //Try to find user photo profile on UserInfo
+        if(profileUri == null){
+            for (UserInfo userInfo : user.getProviderData()) {
+                if (profileUri == null && userInfo.getPhotoUrl() != null) {
+                    profileUri = userInfo.getPhotoUrl();
+                    break;
+                }
+            }
+        }
+
+        mTextViewTextEmail.setText(mUserEmail);
+        mTextViewUsername.setText(mUsername);
+
+        Glide.with(this)
+                .load(profileUri)
+                .centerCrop()
+                .placeholder(android.R.drawable.sym_def_app_icon)
+                .transform(new CircleTransform(mContext))
+                .into(mImageViewProfile);
+
+        Snackbar.make(mConstraintLayout,getText(R.string.success_sign_in),Snackbar.LENGTH_SHORT).show();
+
+    }
+
+    public void onSignedOut(){
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_CANCELED) finish();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 
     @Override
@@ -67,8 +208,18 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch (id){
+
+            case R.id.action_settings:
+
+                return true;
+
+            case R.id.action_sign_out:
+
+                AuthUI.getInstance().signOut(this);
+
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
