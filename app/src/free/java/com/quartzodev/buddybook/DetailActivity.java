2acadaa13684286c955eecfb178401b75e40a130
victoraldir <vhaldir@gmail.com>
2017-06-10 18:21:26 +0100

    Possible a successful build now

---
+++ app/src/free/java/com/quartzodev/buddybook/DetailActivity.java
@@ -0,0 +1,121 @@
+package com.quartzodev.buddybook;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.quartzodev.data.BookApi;
+import com.quartzodev.data.FirebaseDatabaseHelper;
+import com.quartzodev.fragments.DetailActivityFragment;
+import com.quartzodev.utils.DialogUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class DetailActivity extends AppCompatActivity implements
+        DetailActivityFragment.OnDetailInteractionListener,
+        FirebaseDatabaseHelper.OnPaidOperationListener{
+
+    public static final String ARG_BOOK_ID = "bookId";
+    public static final String ARG_FOLDER_ID = "folderId";
+    public static final String ARG_USER_ID = "userId";
+    public static final String ARG_FOLDER_LIST_ID = "folderListId";
+    public static final String ARG_BOOK_JSON = "bookJson";
+    public static final String ARG_FLAG_IS_LENT_BOOK = "isLentBook";
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.detail_coordinator)
+    CoordinatorLayout mCoordinatorLayout;
+    private FirebaseDatabaseHelper mFirebaseDatabaseHelper;
+    private String mUserId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mFirebaseDatabaseHelper = FirebaseDatabaseHelper.getInstance();
+
+        String bookId = getIntent().getExtras().getString(ARG_BOOK_ID);
+        String folderId = getIntent().getExtras().getString(ARG_FOLDER_ID);
+        mUserId = getIntent().getExtras().getString(ARG_USER_ID);
+        String folderListId = getIntent().getExtras().getString(ARG_FOLDER_LIST_ID);
+        String bookJson = getIntent().getExtras().getString(ARG_BOOK_JSON);
+        boolean isLent = getIntent().getExtras().getBoolean(ARG_FLAG_IS_LENT_BOOK);
+
+        DetailActivityFragment newFragment = DetailActivityFragment.newInstance(mUserId, bookId, folderId, folderListId, bookJson, isLent);
+
+        getSupportFragmentManager().popBackStackImmediate();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.detail_container, newFragment).commit();
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                supportFinishAfterTransition();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (getSupportFragmentManager().getBackStackEntryCount() == 0) {
+            supportFinishAfterTransition();
+        } else {
+            getSupportFragmentManager().popBackStack();
+        }
+    }
+
+    @Override
+    public void onLendBook(BookApi bookApi) {
+        DialogUtils.alertDialogLendBook(this, mCoordinatorLayout, mFirebaseDatabaseHelper, mUserId, bookApi);
+    }
+
+    @Override
+    public void onReturnBook(BookApi bookApi) {
+        DialogUtils.alertDialogReturnBook(this, mFirebaseDatabaseHelper, mUserId, bookApi);
+    }
+
+    public void loadBook() {
+        ((DetailActivityFragment) getSupportFragmentManager().findFragmentById(R.id.detail_container)).loadBook();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
+    public void onInsertBook(boolean success) {
+        if(!success){
+            DialogUtils.alertDialogUpgradePro(this);
+        }else{
+            Snackbar.make(mCoordinatorLayout,getString(R.string.insert_success),Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onInsertFolder(boolean success) {
+        if(!success){
+            DialogUtils.alertDialogUpgradePro(this);
+        }
+    }
+}
---
+++ app/src/free/java/com/quartzodev/utils/DialogUtils.java
@@ -0,0 +1,295 @@
+package com.quartzodev.utils;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.quartzodev.buddybook.DetailActivity;
+import com.quartzodev.buddybook.MainActivity;
+import com.quartzodev.buddybook.R;
+import com.quartzodev.data.BookApi;
+import com.quartzodev.data.FirebaseDatabaseHelper;
+import com.quartzodev.data.Folder;
+import com.quartzodev.data.Lend;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by victoraldir on 28/03/2017.
+ */
+
+public class DialogUtils {
+
+    public static void alertDialogDeleteFolder(final Context context, Folder folder, DialogInterface.OnClickListener onClickListener) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+
+        builder.setMessage(R.string.dialog_message)
+                .setTitle(String.format(context.getString(R.string.dialog_title), folder.getDescription()))
+                .setPositiveButton(R.string.dialog_btn_positive, onClickListener)
+                .setNegativeButton(R.string.dialog_btn_cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setCancelable(true);
+
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+
+    }
+
+    public static void alertDialogListFolder(final Context context, String foldersCommaSep, DialogInterface.OnClickListener onClickListener) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+
+        builder.setTitle(R.string.pick_folder)
+                .setNegativeButton(context.getString(R.string.dialog_btn_cancel), null)
+                .setItems(formatFolderList(foldersCommaSep), onClickListener);
+
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.show();
+
+    }
+
+    private static String[] formatFolderList(String folderList) {
+
+        String[] newList = new String[0];
+
+        if(folderList != null) {
+            String[] unFormatedList = folderList.split(",");
+
+            newList = new String[unFormatedList.length];
+
+            for (int x = 0; x < unFormatedList.length; x++) {
+                newList[x] = unFormatedList[x].split("=")[0];
+            }
+
+        }
+
+        return newList;
+    }
+
+    public static void alertDialogAddFolder(final Activity activity,
+                                            final List<Folder> folders,
+                                            final FirebaseDatabaseHelper mFirebaseDatabaseHelper,
+                                            final String userId) {
+
+        LayoutInflater inflater = activity.getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.dialog_add_folder, null);
+
+        final EditText urlEditText = (EditText) view.findViewById(R.id.edittext_add_folder_description);
+        urlEditText.setSingleLine(true);
+        urlEditText.setContentDescription(activity.getString(R.string.receiver_name_cd));
+        final TextInputLayout textInputLayout = (TextInputLayout) view.findViewById(R.id.signup_input_layout_name);
+
+        final AlertDialog dialog = new AlertDialog.Builder(activity)
+                .setTitle(R.string.item_add_folder)
+                .setPositiveButton(activity.getString(R.string.ok), null)
+                .setNegativeButton(activity.getString(R.string.dialog_btn_cancel), null)
+                .setView(view)
+                .create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
+            @Override
+            public void onShow(DialogInterface arg0) {
+
+                Button okButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                okButton.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View arg0) {
+
+                        if (urlEditText.getText().toString().isEmpty()) {
+
+                            textInputLayout.setError(activity.getString(R.string.folder_desc_empty));
+
+                        } else if (folders != null && folders.contains(new Folder(urlEditText.getText().toString()))) {
+
+                            textInputLayout.setError(activity.getString(R.string.folder_already_exits));
+
+                        } else {
+
+                            Folder newFolder = new Folder(urlEditText.getText().toString());
+
+                            if(activity instanceof DetailActivity){
+                                mFirebaseDatabaseHelper.insertFolder(userId, newFolder,(DetailActivity) activity);
+                            }else{
+                                mFirebaseDatabaseHelper.insertFolder(userId, newFolder,(MainActivity) activity);
+                            }
+
+                            dialog.dismiss();
+                        }
+
+                    }
+                });
+            }
+        });
+
+        dialog.show();
+
+    }
+
+    public static void alertDialogLendBook(final Activity activity,
+                                           final CoordinatorLayout coordinatorLayout,
+                                           final FirebaseDatabaseHelper mFirebaseDatabaseHelper,
+                                           final String userId,
+                                           final BookApi book) {
+
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_lend_book, null);
+
+        final AlertDialog dialog = new AlertDialog.Builder(activity)
+                .setTitle(R.string.dialog_title_lend)
+                .setPositiveButton(activity.getString(R.string.ok), null)
+                .setNegativeButton(activity.getString(R.string.dialog_btn_cancel), null)
+                .setView(view)
+                .create();
+
+        final EditText nameEdtText = (EditText) view.findViewById(R.id.edittext_receiver_name);
+        nameEdtText.setSingleLine(true);
+        nameEdtText.setContentDescription(activity.getString(R.string.receiver_name_cd));
+
+        final TextInputLayout nameInputLayout = (TextInputLayout) view.findViewById(R.id.dialog_input_layout_name);
+
+        final EditText emailEdtText = (EditText) view.findViewById(R.id.edittext_receiver_email);
+        emailEdtText.setSingleLine(true);
+        emailEdtText.setContentDescription(activity.getString(R.string.receiver_email_cd));
+
+        final TextInputLayout emailInputLayout = (TextInputLayout) view.findViewById(R.id.dialog_input_layout_email);
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
+            @Override
+            public void onShow(DialogInterface arg0) {
+
+                Button okButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                okButton.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View arg0) {
+
+                        if (nameEdtText.getText().toString().isEmpty()) {
+
+                            nameInputLayout.setError(activity.getString(R.string.lend_name_empty));
+
+                        } else if (emailEdtText.getText().toString().isEmpty()) {
+
+                            emailInputLayout.setError(activity.getString(R.string.lend_email_empty));
+
+                        }else if(!isValidEmail(emailEdtText.getText().toString())) {
+                            emailInputLayout.setError(activity.getString(R.string.lend_email_invalid));
+                        }else {
+
+                            Lend lend = new Lend(nameEdtText.getText().toString(),
+                                    emailEdtText.getText().toString(),
+                                    new Date());
+
+                            book.setLend(lend);
+
+                            mFirebaseDatabaseHelper.updateBook(userId, FirebaseDatabaseHelper.REF_MY_BOOKS_FOLDER, book);
+
+                            try {
+                                DetailActivity detailActivity = ((DetailActivity) activity);
+
+                                if (detailActivity != null) {
+                                    detailActivity.loadBook();
+                                }
+                            } catch (Exception ex) {
+                            }
+
+                            dialog.dismiss();
+
+                            Snackbar.make(coordinatorLayout, activity.getText(R.string.success_lend_book), Snackbar.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+        });
+
+        dialog.show();
+
+    }
+
+    public final static boolean isValidEmail(CharSequence target) {
+        if (target == null) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+
+    public static void alertDialogReturnBook(final Activity activity,
+                                             final FirebaseDatabaseHelper mFirebaseDatabaseHelper,
+                                             final String userId,
+                                             final BookApi book) {
+
+        final AlertDialog dialog = new AlertDialog.Builder(activity)
+                .setTitle(activity.getString(R.string.dialog_return_book_title))
+                .setMessage(String.format(activity.getString(R.string.dialog_return_book_body), book.getLend().getReceiverName()))
+                .setPositiveButton(activity.getString(R.string.dialog_btn_positive), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        BookApi updatedBook = book;
+                        updatedBook.setLend(null);
+                        mFirebaseDatabaseHelper.updateBook(userId, FirebaseDatabaseHelper.REF_MY_BOOKS_FOLDER, updatedBook);
+
+                        try {
+                            ((DetailActivity) activity).loadBook();
+                        } catch (Exception ex) {
+                        }
+                    }
+                })
+                .setNegativeButton(activity.getString(R.string.dialog_btn_cancel), null)
+                .setCancelable(true)
+                .create();
+
+        dialog.show();
+
+    }
+
+    public static void alertDialogUpgradePro(final Activity activity){
+
+        final AlertDialog dialog = new AlertDialog.Builder(activity)
+                .setTitle(R.string.dialog_upgrade)
+                .setMessage(R.string.dialog_upgrade_message)
+                .setPositiveButton(R.string.dialog_btn_updgate, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        final String appPackageName = activity.getPackageName(); // getPackageName() from Context or Activity object
+                        try {
+                            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                        } catch (android.content.ActivityNotFoundException anfe) {
+                            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                        }
+
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton(activity.getString(R.string.dialog_btn_cancel),null)
+                .setCancelable(true)
+                .create();
+
+        dialog.show();
+    }
+}
