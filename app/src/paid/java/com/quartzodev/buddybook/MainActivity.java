cddee271b5c913a45b509e6f8f13d527e5d35a2b
victoraldir <vhaldir@gmail.com>
2017-06-28 14:17:39 +0100

    Removing unused imports

--- app/src/paid/java/com/quartzodev/buddybook/MainActivity.java
+++ app/src/paid/java/com/quartzodev/buddybook/MainActivity.java
@@ -19,7 +19,6 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.MenuItemCompat;
