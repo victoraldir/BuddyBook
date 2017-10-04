17cc76071b68259255b6aced6b6a59892185f4cb
victoraldir <vhaldir@gmail.com>
2017-10-04 20:27:01 -0300

    Fix floating background lines problem

--- app/src/main/java/com/quartzodev/views/LinedEditText.java
+++ app/src/main/java/com/quartzodev/views/LinedEditText.java
@@ -2,11 +2,14 @@ package com.quartzodev.views;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.widget.EditText;
+
+import com.quartzodev.buddybook.R;
 
 /**
  * Created by victoraldir on 03/10/2017.
@@ -14,91 +17,39 @@ import android.widget.EditText;
 
 public class LinedEditText extends android.support.v7.widget.AppCompatEditText {
 
-    // the vertical offset scaling factor (10% of the height of the text)
-    private static final float VERTICAL_OFFSET_SCALING_FACTOR = 0.1f;
+    private Rect mRect;
+    private Paint mPaint;
 
-    // the dashed line scale factors
-    private static final float DASHED_LINE_ON_SCALE_FACTOR = 0.008f;
-    private static final float DASHED_LINE_OFF_SCALE_FACTOR = 0.0125f;
-
-    // the paint we will use to draw the lines
-    private Paint dashedLinePaint;
-
-    // a reusable rect object
-    private Rect reuseableRect;
-
-    public LinedEditText(Context context) {
-        super(context);
-        init();
-    }
-
+    // we need this constructor for LayoutInflater
     public LinedEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
-    }
 
-    public LinedEditText(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init();
-    }
-
-    private void init() {
-
-        // instantiate the rect
-        reuseableRect = new Rect();
-
-        // instantiate the paint
-        dashedLinePaint = new Paint();
-        dashedLinePaint.setARGB(200, 0, 0, 0);
-        dashedLinePaint.setStyle(Paint.Style.STROKE);
+        mRect = new Rect();
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        mPaint.setColor(Color.BLACK); //SET YOUR OWN COLOR HERE
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        //int count = getLineCount();
 
-        // set the path effect based on the view width
-        dashedLinePaint.setPathEffect(
-                new DashPathEffect(
-                        new float[]{
-                                getWidth() * DASHED_LINE_ON_SCALE_FACTOR,
-                                getWidth() * DASHED_LINE_OFF_SCALE_FACTOR},
-                        0));
-
-        // get the height of the view
         int height = getHeight();
+        int line_height = getLineHeight();
 
-        // get the height of each line (not subtracting one from the line height makes lines uneven)
-        int lineHeight = getLineHeight() - 1;
+        int count = height / line_height;
 
-        // set the vertical offset basef off of the view width
-        int verticalOffset = (int) (lineHeight * VERTICAL_OFFSET_SCALING_FACTOR);
+        if (getLineCount() > count)
+            count = getLineCount();//for long text with scrolling
 
-        // the number of lines equals the height divided by the line height
-        int numberOfLines = height / lineHeight;
+        Rect r = mRect;
+        Paint paint = mPaint;
+        int baseline = getLineBounds(0, r);//first line
 
-        // if there are more lines than what appear on screen
-        if (getLineCount() > numberOfLines) {
+        for (int i = 0; i < count; i++) {
 
-            // set the number of lines to the line count
-            numberOfLines = getLineCount();
-        }
-
-        // get the baseline for the first line
-        int baseline = getLineBounds(0, reuseableRect);
-
-        // for each line
-        for (int i = 0; i < numberOfLines; i++) {
-
-            // draw the line
-            canvas.drawLine(
-                    reuseableRect.left,             // left
-                    baseline + verticalOffset,      // top
-                    reuseableRect.right,            // right
-                    baseline + verticalOffset,      // bottom
-                    dashedLinePaint);               // paint instance
-
-            // get the baseline for the next line
-            baseline += lineHeight;
+            canvas.drawLine(r.left, baseline + 1, r.right, baseline + 1, paint);
+            baseline += getLineHeight();//next line
         }
 
         super.onDraw(canvas);
