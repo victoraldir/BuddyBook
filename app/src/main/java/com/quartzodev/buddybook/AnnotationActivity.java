8e295f6070096db868989c419cef65ef14437703
victoraldir <vhaldir@gmail.com>
2017-10-04 20:29:10 -0300

    Optimizing imports

--- app/src/main/java/com/quartzodev/buddybook/AnnotationActivity.java
+++ app/src/main/java/com/quartzodev/buddybook/AnnotationActivity.java
@@ -3,16 +3,12 @@ package com.quartzodev.buddybook;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.Selection;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
